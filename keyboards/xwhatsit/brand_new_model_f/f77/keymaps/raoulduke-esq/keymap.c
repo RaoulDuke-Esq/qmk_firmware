83b7f284d9e0e762a7418513960801050f56ac37
raoulduke-esq <mgmobile@gmail.com>
2021-05-07 19:54:29 -0400

    New layout for F77

--- keyboards/xwhatsit/brand_new_model_f/f77/keymaps/raoulduke-esq/keymap.c
+++ keyboards/xwhatsit/brand_new_model_f/f77/keymaps/raoulduke-esq/keymap.c
@@ -15,6 +15,24 @@
  */
 #include QMK_KEYBOARD_H
 
+#define LOCK LGUI(LCTL(KC_Q))
+#define MMIC HYPR(KC_M)
+#define DICT HYPR(KC_D)
+#define PTRS LGUI(KC_BSPC)
+#define ETRS LGUI(LALT(LSFT(KC_BSPC)))
+#define INFO LGUI(LCTL(KC_I))
+#define DLS LGUI(LALT(KC_L))
+#define SCAP LGUI(LSFT(KC_4))
+#define SCRN LGUI(LSFT(KC_5))
+#define PSTE LGUI(LALT(LSFT(KC_V)))
+#define WLFT LALT(KC_LEFT)
+#define WRTE LALT(KC_RIGHT)
+#define DLFT LALT(KC_BSPC)
+#define DRTE LALT(KC_DEL)
+#define QUE LGUI(LSFT(KC_B))
+#define SLFT LGUI(KC_LEFT)
+#define SRTE LGUI(KC_RIGHT)
+
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
@@ -23,29 +41,26 @@ enum layer_names {
 };
 
 // Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    QMKBEST = SAFE_RANGE,
-    QMKURL
-};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT_ansi_hhkb_split_shift_split_backspace(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,    KC_P7,   KC_P8,   KC_P9,
-        KC_TAB,    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,        KC_P4,   KC_P5,   KC_P6,
-        KC_LCTRL,     KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,            KC_P1,   KC_P2,   KC_P3,
-        KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),   KC_P0,   KC_UP,   KC_PDOT,
-        KC_CAPS, KC_LGUI, KC_LALT,                      KC_SPC,                  KC_RALT, KC_NLCK, KC_RCTRL,     KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,    PSTE,     KC_HOME,  KC_PGUP,
+        KC_TAB,    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,        KC_DEL,   KC_END,   KC_PGDN,
+        KC_LGUI,      KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,            KC_MPRV,  KC_MPLY,  KC_MNXT,
+        KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),   KC_VOLD,  KC_UP,    KC_VOLU,
+        KC_LCTL, KC_LGUI, KC_LALT,                      KC_SPC,                            DICT, LOCK, MMIC,     KC_LEFT,  KC_DOWN,  KC_RIGHT
     ),
     [_FN] = LAYOUT_ansi_hhkb_split_shift_split_backspace(
-        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9, KC_F10,    KC_F11, KC_F12, KC_INS, KC_DEL,    KC_HOME,   KC_UP,   KC_PGUP,
-        KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, KC_NO, KC_DEL,          KC_LEFT,   KC_NO,   KC_RIGHT,
-        _______,     KC_VOLD, KC_VOLU, KC_MUTE, KC_EJCT, KC_NO, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, KC_NO,  KC_END,    KC_DOWN, KC_PGDN,
-        _______,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, _______, _______,      KC_INS,    KC_NO,   KC_DEL,
-        _______, KC_NO, _______,                             MO(_FN2),                           _______, KC_NO, _______,      KC_NO,     KC_NO,   KC_NO
+        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9, KC_F10,    KC_F11, KC_F12, KC_INS, KC_DEL,    PTRS,  INFO, SCAP,
+        KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, KC_NO, KC_DEL,          ETRS,  DLS,  SCRN,
+        _______,     KC_VOLD, KC_VOLU, KC_MUTE, KC_EJCT, KC_NO, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, KC_NO,  DLFT,  QUE,  DRTE,
+        _______,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, _______, _______,      WLFT,  KC_F3,   WRTE,
+        _______, KC_NO, _______,                             MO(_FN2),                           _______, KC_NO, _______,      SLFT,  KC_F13,   SRTE
     ),
     [_FN2] = LAYOUT_ansi_hhkb_split_shift_split_backspace(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
+        RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
         _______,     _______, _______, EEPROM_RESET, RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______,
         _______,         _______, _______, DEBUG, _______, _______, _______, _______, _______, _______, _______, _______, _______,                _______, _______, _______,
         _______,             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
@@ -53,27 +68,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best thing ever!");
-            } else {
-                // when keycode QMKBEST is released
-            }
-            break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/\n");
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-    }
-    return true;
-}
 
 /*
 void matrix_init_user(void) {
