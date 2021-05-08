894c2e304f3d95c664ed4a69d7c07858ea747683
raoulduke-esq <mgmobile@gmail.com>
2021-05-08 10:55:04 -0400

    Activate ADB converter console

--- keyboards/converter/adb_usb/rules.mk
+++ keyboards/converter/adb_usb/rules.mk
@@ -16,7 +16,7 @@ BOOTLOADER = caterina
 #
 BOOTMAGIC_ENABLE = no   # Virtual DIP switch configuration
 MOUSEKEY_ENABLE  = no   # Mouse keys
-CONSOLE_ENABLE   = no   # Console for debug
+CONSOLE_ENABLE   = yes   # Console for debug
 COMMAND_ENABLE   = no   # Commands for debug and configuration
 SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE      = no   # USB Nkey Rollover - not yet supported in LUFA
