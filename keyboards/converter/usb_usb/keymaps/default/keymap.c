020b65a29ac26782e1ee7101d296db5fcf03730c
raoulduke-esq <mgmobile@gmail.com>
2021-06-14 19:25:50 -0400

    Update add USB-USB keymap

--- keyboards/converter/usb_usb/keymaps/default/keymap.c
+++ keyboards/converter/usb_usb/keymaps/default/keymap.c
@@ -17,6 +17,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#define LOCK LGUI(LCTL(KC_Q))
+#define MMIC HYPR(KC_M)
+#define DICT HYPR(KC_D)
+#define PTRS LGUI(KC_BSPC)
+#define ETRS LGUI(LALT(LSFT(KC_BSPC)))
+#define INFO LGUI(LCTL(KC_I))
+#define DLS LGUI(LALT(KC_L))
+#define SCAP LGUI(LSFT(KC_4))
+#define SCRN LGUI(LSFT(KC_5))
+#define PSTE LGUI(LALT(LSFT(KC_V)))
+#define WLFT LALT(KC_LEFT)
+#define WRTE LALT(KC_RIGHT)
+#define DLFT LALT(KC_BSPC)
+#define DRTE LALT(KC_DEL)
+#define QUE LGUI(LSFT(KC_B))
+#define SLFT LCTL(KC_LEFT)
+#define SRTE LCTL(KC_RIGHT)
+#define SPK HYPR(KC_S)
+#define COPY LGUI(KC_C)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: plain Qwerty without layer switching
      *         ,---------------. ,---------------. ,---------------.
@@ -38,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT_all(
                       KC_F13,  KC_F14,  KC_F15,  KC_F16, KC_F17, KC_F18, KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,
-    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,               KC_PSCR, KC_SLCK, KC_PAUS,    KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR,     KC_HELP,
+    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  PTRS,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  ETRS,               KC_PSCR, KC_SLCK, KC_PAUS,    KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR,     KC_HELP,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_JYEN, KC_BSPC,     KC_INS,  KC_HOME, KC_PGUP,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,    KC_STOP, KC_AGIN,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,     KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,    KC_MENU, KC_UNDO,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_NUHS, KC_ENT,                                    KC_P4,   KC_P5,   KC_P6,   KC_PCMM,    KC_SLCT, KC_COPY,
